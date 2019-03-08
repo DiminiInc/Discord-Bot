3cf4944a860e95d2220dc828a4482fb3e09b932d
Yaskovich Dmitry <kadavr95@outlook.com>
2019-03-08 23:32:17 +0300

    added port

--- bot.js
+++ bot.js
@@ -54,3 +54,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
